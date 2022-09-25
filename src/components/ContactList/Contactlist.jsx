4831f488c5923cfbba388ee93f2255e2928f1980
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-09-25 10:16:37 +0300

    updated_5

--- src/components/ContactList/Contactlist.jsx
+++ src/components/ContactList/Contactlist.jsx
@@ -5,29 +5,27 @@ import css from './ContactList.module.css';
 export const ContactList = ({data, onDeleteContact}) => {
     return (
       <ul className={css.contactList}>
-          {data.length !== 0 ? (
-            data.map(dataItem => {
-            return (
-            <ContactItem
-                key={dataItem.id}
-                id={dataItem.id}
-                name={dataItem.name}
-                number={dataItem.number}
-                onDeleteContact={onDeleteContact}   
-            />
-            )}))
-            : (<li>There is no one contact</li>)}
+        {data.map(dataItem => {
+        return (
+        <ContactItem
+            key={dataItem.id}
+            id={dataItem.id}
+            name={dataItem.name}
+            number={dataItem.number}
+            onDeleteContact={onDeleteContact}   
+        />
+        )})}
       </ul>
-    )
-}
+    );
+  };
 
 
-ContactList.propTypes = {
-  data: PropTypes.arrayOf(
-    PropTypes.exact({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  })),
-  onDeleteContact: PropTypes.func.isRequired,
-}
+  ContactList.propTypes = {
+    data: PropTypes.arrayOf(
+      PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })),
+    onDeleteContact: PropTypes.func.isRequired,
+  }
