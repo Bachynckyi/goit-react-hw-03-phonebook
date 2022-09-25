390950cfc6435a7627a9b041494aca134194a58d
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-09-25 09:44:20 +0300

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,26 +8,13 @@ import { Filter } from "./Filter/Filter";
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  };
-
-  componentDidMount() {
-    const savedContact = (localStorage.getItem('contacts'));
-    const contact = JSON.parse(savedContact);
-    this.setState({contacts: contact})
-  };
-
   onSubmitHandler = data => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, data],
